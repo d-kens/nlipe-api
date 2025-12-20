2bad85e3803d5da80dfe9efbd5c49d38e831773b
dickensomondionyango <d.onyango@carepay.com>
2025-12-20 10:48:10 +0300

    Initial commit

---
+++ src/main/java/com/nlipe/nlipe/NlipeApplication.java
@@ -0,0 +1,13 @@
+package com.nlipe.nlipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nlipe/nlipe/NlipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlipe.nlipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
